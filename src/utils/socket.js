87aef5d0fa83f2be66562ad02d7a8a000b7256cb
Murari <kothamasuchandrasai56@gmail.com>
2025-12-15 09:21:29 +0530

    Save chat messages to database

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,5 @@
-const socket = require("socket.io");
+const { Chat } = require("../models/chat");
+
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
@@ -16,9 +17,34 @@ const initializeSocket = (server) => {
     });
     
 
-    socket.on("sendMessage", ({firstName , lastName , userId , targetUserId , text}) => {
-      const roomId = [userId , targetUserId].sort().join("_");
-      io.to(roomId).emit("messageReceived" , {firstName , lastName ,  text})
+    socket.on("sendMessage", async ({firstName , lastName , userId , targetUserId , text}) => {
+      // Save message to DB
+      try {
+        const roomId = [userId , targetUserId].sort().join("_");
+        console.log(firstName + " " + text);
+        
+        let chat = await Chat.findOne({
+          participants: { $all: [userId, targetUserId] },
+        });
+
+        if (!chat) {
+          chat = new Chat({
+            participants: [userId, targetUserId],
+            messages: [],
+          });
+        }
+
+        chat.messages.push({
+          senderId: userId,
+          text,
+        });
+
+        await chat.save();
+        
+        io.to(roomId).emit("messageReceived" , {firstName , lastName ,  text})
+      } catch (err) {
+        console.log(err);
+      }
     });
     
     socket.on("disconnect", () => {});
